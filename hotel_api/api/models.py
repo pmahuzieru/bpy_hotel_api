b778c187f944d3194f3ac4d7fc346497094f6903
Pablo <pmahuzieru@gmail.com>
2024-12-09 19:51:00 -0300

    Add Room and Reservation models

--- hotel_api/api/models.py
+++ hotel_api/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=2000)
+    price_per_night = models.DecimalField(decimal_places=2)
+    is_available = models.BooleanField()
+
+    def __str__(self):
+        return f"Room [{self.name}]"
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.Cascade, related_name="reservations"
+    )
+    room = models.ForeignKey(
+        Room, on_delete=models.Cascade, related_name="reservations"
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"Reservation {self.room}, {self.start_date} - {self.end_date}"
