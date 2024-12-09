598a8b88a2aaec480903072a87f5977a1d1b8812
rypptc <rypptc@gmail.com>
2024-12-10 00:28:22 +0100

    added reservation view set

--- hotel_api/api/views.py
+++ hotel_api/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Reservation
+from .serializers import ReservationSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
-# Create your views here.
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = [IsAuthenticated]
