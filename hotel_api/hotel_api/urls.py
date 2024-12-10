78c80228230ec1d0cb4c0f6a3a06e5229ea5f12f
Pablo <pmahuzieru@gmail.com>
2024-12-09 21:16:39 -0300

    Add Router in URLs

--- hotel_api/hotel_api/urls.py
+++ hotel_api/hotel_api/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views import RoomViewSet, ReservationViewSet
+
+router = DefaultRouter()
+router.register('rooms', RoomViewSet)
+router.register('reservations', ReservationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token/', obtain_auth_token, name='api_token'),    
+    path('api-token/', obtain_auth_token, name='api_token'), 
+    path('', include(router.urls)),   
 ]
