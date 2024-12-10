56e66d0a1e5bcf225dc524064e39bc09e422a427
rypptc <rypptc@gmail.com>
2024-12-10 01:39:23 +0100

    added docstrings

--- hotel_api/api/models.py
+++ hotel_api/api/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Room(models.Model):
+    """
+    Representa una habitacion de hotel
+
+    Atributos
+    - name (str): el nombre de la habitacion
+    - description (str): la descripcion de la habitacion
+    - price_per_night (Decimal): el precio
+    - is_available (bool): si esta o no disponible
+    """
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=2000)
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
@@ -14,6 +23,16 @@ class Room(models.Model):
 
 
 class Reservation(models.Model):
+    """
+    Representa una reservacion realizada por un usuario.
+
+    Atributos:
+        user (User): el usuario que hace la reservacion.
+        room (Room): la habitacion que fue reservada.
+        start_date (Date): fecha de inicio.
+        end_date (Date): fecha de fin.
+    """
+
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="reservations"
     )
