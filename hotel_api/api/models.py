601278eef95ed49dc5f03d9bf57c3ddf234ab2d5
rypptc <rypptc@gmail.com>
2024-12-10 01:19:11 +0100

    update model room

--- hotel_api/api/models.py
+++ hotel_api/api/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Room(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=2000)
-    price_per_night = models.DecimalField(decimal_places=2)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     is_available = models.BooleanField()
 
     def __str__(self):
