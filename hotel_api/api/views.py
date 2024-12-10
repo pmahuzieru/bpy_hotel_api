0cde21796ca895f2d43e64b41938ee05c699126c
rypptc <rypptc@gmail.com>
2024-12-10 01:46:30 +0100

    added docstrings to viewsets

--- hotel_api/api/views.py
+++ hotel_api/api/views.py
@@ -6,12 +6,36 @@ from rest_framework import viewsets
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
+    """
+    API para gestionar las reservas.
+
+    Metodos soportados:
+    - GET: Lista todas las reservas.
+    - POST: Crea una nueva reserva.
+    - PUT: Actualiza una reserva existente.
+    - DELETE: Elimina una reserva.
+
+    Permisos:
+    - Solo los usuarios autenticados que no sean administradores pueden acceder.
+    """
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
     permission_classes = [IsAuthenticated, NotAdminUser]
 
 
 class RoomViewSet(viewsets.ModelViewSet):
+    """
+    API para gestionar las habitaciones.
+
+    Metodos soportados:
+    - GET: Lista todas las habitaciones.
+    - POST: Crea una nueva habitacion.
+    - PUT: Actualiza una habitacion existente.
+    - DELETE: Elimina una habitacion.
+
+    Permisos:
+    - Solo los administradores autenticados pueden acceder.
+    """
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
