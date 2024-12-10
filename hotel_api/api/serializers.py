20647ea09d3fce97e9149a95e2562dad1d3cd49c
Pablo <pmahuzieru@gmail.com>
2024-12-09 21:50:32 -0300

    Validate end date Reservation

--- hotel_api/api/serializers.py
+++ hotel_api/api/serializers.py
@@ -7,6 +7,17 @@ class ReservationSerializer(serializers.ModelSerializer):
         model = Reservation
         fields = '__all__'
         
+    def validate_end_date(self, value):
+        """
+        Check that end date is after start date
+        """
+        start_date = self.initial_data.get("start_date")
+        
+        if start_date:
+            start_date = serializers.DateField().to_internal_value(start_date)
+            if value <= start_date:
+                raise serializers.ValidationError("Reservation end date must be later than start date.")
+        
         
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
